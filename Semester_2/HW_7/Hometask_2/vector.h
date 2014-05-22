12fe7481e022b823b566a204164029782205f35e
Eldar <ric1995@mail.ru>
2014-05-22 20:13:46 +0400

    7_2 исправлено, есть вопрос

--- Semester_2/HW_7/Hometask_2/vector.h
+++ Semester_2/HW_7/Hometask_2/vector.h
@@ -10,88 +10,116 @@ public:
     /**
      * @brief constructor that do not set vector's coordinates
      */
-    Vector()
-    {
-        dimension = dim;
-        coordinates = new T[dim];
-    }
+    Vector();
 
     /**
      * @brief constructor that sets coordinate value to all vector's coordinates
      */
-    Vector(T coordinate)
-    {
-        dimension = dim;
-        coordinates = new T[dim];
-        for (int i = 0; i < dim; i++)
-            coordinates[i] = coordinate;
-    }
+    Vector(const T &coordinate);
 
     /**
      * @brief constructor that sets i-param to vector's i-coordinate
      */
-    Vector(T coordinate1, T coordinate2,...)
-    {
-        T* curr = &coordinate1;
-        dimension = dim;
-        coordinates = new T[dim];
-        for (int i = 0; i < dim; i++)
-        {
-            coordinates[i] = *curr;
-            curr++;
-        }
-    }
+    Vector(T coordinate1, T coordinate2, ...);
 
     /**
      * @return vector - result of first and second vectors' sum
      */
-    friend Vector operator +(Vector<T, dim> &first, Vector<T, dim> &second)
-    {
-        Vector<T, dim> tmp;
-        for (int i = 0; i < dim; i++)
-            tmp.coordinates[i] = first.coordinates[i] + second.coordinates[i];
-        return tmp;
-    }
+    Vector operator +(const Vector<T, dim> &second);
+
+    ~Vector();
 
     /**
      * @return T-variable - result of first and second vectors' scalar multiplication
      */
-    friend T operator *(Vector<T, dim> &first, Vector<T, dim> &second)
-    {
-        T tmp = 0;
-        for (int i = 0; i < dim; i++)
-            tmp = tmp + (first.coordinates[i] * second.coordinates[i]);
-        return tmp;
-    }
+    T operator *(const Vector<T, dim> &second);
 
 
     /**
      * @return vector's i-coordinate
      */
-    T getDimensionValue(unsigned int i)
-    {
-        return coordinates[i];
-    }
+    T getDimensionValue(unsigned int i);
 
     /**
      * @return true if the vector is equal to null-vector; otherwise returns false.
      */
-    bool isNullVector()
-    {
-        bool tmp = true;
-        for (int i = 0; i < dimension; i++)
-        {
-            if (coordinates[i] != 0)
-            {
-                tmp = false;
-                break;
-            }
-        }
-        return tmp;
-    }
+    bool isNullVector();
 
 private:
     T *coordinates;
 
     unsigned int dimension;
 };
+
+
+template <typename T, unsigned int dim>
+Vector<T, dim>::Vector()
+{
+    dimension = dim;
+    coordinates = new T[dim];
+}
+
+template <typename T, unsigned int dim>
+Vector<T, dim>::Vector(const T &coordinate)
+{
+    dimension = dim;
+    coordinates = new T[dim];
+    for (int i = 0; i < dim; i++)
+        coordinates[i] = coordinate;
+}
+
+template <typename T, unsigned int dim>
+Vector<T, dim>::Vector(T coordinate1, T coordinate2, ...)
+{
+    T *curr = &coordinate1;
+    dimension = dim;
+    coordinates = new T[dim];
+    for (int i = 0; i < dim; i++)
+    {
+        coordinates[i] = *curr;
+        curr++;
+    }
+}
+
+template <typename T, unsigned int dim>
+Vector<T, dim> Vector<T, dim>::operator +(const Vector<T, dim> &second)
+{
+    Vector<T, dim> tmp;
+    for (int i = 0; i < dim; i++)
+        tmp.coordinates[i] = this->coordinates[i] + second.coordinates[i];
+    return tmp;
+}
+
+template <typename T, unsigned int dim>
+Vector<T, dim>::~Vector()
+{
+    delete[] coordinates;
+}
+
+template <typename T, unsigned int dim>
+T Vector<T, dim>::operator *(const Vector<T, dim> &second)
+{
+    T tmp = 0;
+    for (int i = 0; i < dim; i++)
+        tmp = tmp + (this->coordinates[i] * second.coordinates[i]);
+    return tmp;
+}
+
+template <typename T, unsigned int dim>
+T Vector<T, dim>::getDimensionValue(unsigned int i)
+{
+    return coordinates[i];
+}
+
+template <typename T, unsigned int dim>
+bool Vector<T, dim>::isNullVector()
+{
+    for (int i = 0; i < dimension; i++)
+    {
+        if (coordinates[i] != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
