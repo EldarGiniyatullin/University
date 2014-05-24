ea6ed59023756019303dfaaf5ec82dbd9a41c44b
Eldar <ric1995@mail.ru>
2014-05-24 17:47:56 +0400

    5_2 комментарии

--- Semester_2/HW_5/Hometask_2/set.h
+++ Semester_2/HW_5/Hometask_2/set.h
@@ -3,28 +3,63 @@
 #include <QList>
 
 template <typename T>
+
+/**
+ * @brief The Set class реаализует множество неповторяющихся элементов типа T
+ */
 class Set
 {
 public:
+    /**
+     * @brief Set - конструктор, создающиий пустое множество
+     */
     Set();
 
     ~Set();
 
+    /**
+     * @brief addValue - добавить элемент в множество
+     * @param value - элемент для добавления
+     */
     void addValue(T value);
 
+    /**
+     * @brief doesSetContain проверяет, принадлежит ли элемент value множеству
+     * @return true/false, если принадлежит/не принадлежит
+     */
     bool doesSetContain(T value);
 
+    /**
+     * @brief deleteValue - исключить элемент value из множества (если существует)
+     */
     void deleteValue(T value);
 
+    /**
+     * @brief getSize - получить количество элементов в множестве
+     */
     unsigned int getSize();
 
+    /**
+     * @brief operator + получить объединение двух множеств
+     * @return третье множество - объединение множеств
+     */
     Set<T> *operator +(Set<T> &second);
 
+    /**
+     * @brief operator * получить пересечение двух множеств
+     * @return третье множество - пересечение множеств
+     */
     Set<T> *operator *(Set<T> &second);
 
 protected:
+    /**
+     * @brief data - список элементов
+     */
     QList<T> *data;
 
+    /**
+     * @brief size - количество элементов в множестве
+     */
     unsigned int size;
 };
 
