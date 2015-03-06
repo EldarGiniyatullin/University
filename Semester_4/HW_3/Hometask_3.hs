b9a1d7f4c596452cb6286f2848645381b93ca927
Eldar <ric1995@mail.ru>
2015-03-07 03:10:51 +0400

    HW_3, task 3
    minBound, (Num a, Ord a) -> Int

--- Semester_4/HW_3/Hometask_3.hs
+++ Semester_4/HW_3/Hometask_3.hs
@@ -1,8 +1,11 @@
-counter1 :: (Ord a, Num a) => [a] -> a -> Integer -> Integer -> Integer
+counter1 :: [Int] -> Int -> Int -> Int -> Int
 counter1 [] acc pos curr = pos
 counter1 [a] acc pos curr = pos
 counter1 [a, b] acc pos curr = pos
 counter1 (x:y:z:xs) acc pos curr = if ((x + z) > acc) then (counter1 (y:z:xs) (x + z) (pos + curr) 1) else (counter1 (y:z:xs) acc pos (curr + 1))
 
-counter :: (Ord a, Num a) => [a] -> Integer
-counter xs = counter1 xs 0 1 1
+counter :: [Int] -> Int
+counter [] = 0
+counter [a] = 0
+counter [a, b] = 0
+counter xs = counter1 xs (minBound :: Int) 1 1
